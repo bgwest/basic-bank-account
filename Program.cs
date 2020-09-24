752f1e48397a4bb5f820694aa1f134c239e3ddf0
Benjamin West <23172276+bgwest@users.noreply.github.com>
2020-09-23 21:15:41 -0700

    bank account code with console outputs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace oopintrotoclasses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var account = new BankAccount("Vinicio", 1000000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.\n");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            // TODO: anyway to make the withdraw amount and note item variables?
+            Console.WriteLine($"Account {account.Number} withdrew 120 for hammock\n");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            // Uncomment to test exceptions
+            //try
+            //{
+            //    var invaildAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.Write("Exception caught creating account with negative balance.");
+            //    Console.Write(e.ToString());
+            //}
+
+            Console.WriteLine($"\nNew account balance: {account.Balance}");
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oopintrotoclasses
+{
+    class Transaction
+    {
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; }
+
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
